c740eb224469c684f8c5395575d3f3da167403c3
Artem033030 <apilipcuk114@gmail.com>
2020-03-24 16:09:31 +0200

    Test

--- Errors.py
+++ Errors.py
@@ -1,13 +1,3 @@
-
-for x in range (-15,15):
-    try:
-        print(30/x)
-        print("значения х ", x)
-        print(type (x))
-        print("15" + x)
-    except ZeroDivisionError:
-        print("Ну ну ну.Не делите на ноль")
-        continue
-    except TypeError:
-        print("15" + str(x))
-        #отлавливать:  , , , 
+Aplle = [1,2,3,4]
+Mac =Aplle[::-1]
+print(Mac)
